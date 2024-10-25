2a7d31e80828218d6120a45aa9b4db4083b7c9f8
Florent Giov annone <giovannoneflorent@gmail.com>
2024-10-25 20:51:32 +0100

    finalised Model, sterelizer and controller for the user logins

---
+++ seed.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, request, jsonify, g
+from app import app, db
+from models.users_model import UserModel
+
+with app.app_context():
+
+    try:
+        print("connected")
+        db.drop_all()
+        db.create_all()
+
+        Florent = UserModel(
+            firstname="Florent",
+            lastname="Giovannone",
+            username="flo",
+            email="f.giovannone@me.com",
+            password="Hello123",
+            password_confirmation="Hello123",
+        )
+
+        db.session.add(Florent)
+        db.session.commit()
+
+        db.session.commit()
+        print("seeded!")
+
+    except Exception as e:
+        print(e)
