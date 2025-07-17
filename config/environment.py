d470d72cf13c99a91760823a544db5eae32e8466
Florent <florent.giovannone@abeta.co.uk>
2025-07-17 14:42:41 +0100

    Add flask-mail dependency

--- config/environment.py
+++ config/environment.py
@@ -1,7 +1,7 @@
 import os
 
-db_URI = os.getenv("DATABASE_URL")
-SECRET = os.getenv("SECRET")
+db_URI = os.getenv("DATABASE_URL", "postgresql://localhost:5432/log_db")
+SECRET = os.getenv("SECRET", "t3cbPSmNgXn-bZ0wGxR8n0fpMC0")
 
-if db_URI.startswith("postgres://"):
+if db_URI and db_URI.startswith("postgres://"):
     db_URI = db_URI.replace("postgres://", "postgresql://", 1)
